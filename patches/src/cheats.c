d22a21aed86ce8580d5eb03544833d9fda16d9d0
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-01-28 00:39:15 -0500

    DLL modding support

--- patches/src/cheats.c
+++ patches/src/cheats.c
@@ -3,11 +3,11 @@
 #include "recomp_funcs.h"
 #include "common.h"
 
-void func_8004D1D4(s32 warpIndex, s32 param2);
+void warpPlayer(s32 warpIndex, s32 param2);
 
 void handle_warp_cheat() {
     s32 pendingWarp = recomp_get_pending_warp();
     if (pendingWarp != -1) {
-        func_8004D1D4(pendingWarp, 1);
+        warpPlayer(pendingWarp, 1);
     }
 } 
