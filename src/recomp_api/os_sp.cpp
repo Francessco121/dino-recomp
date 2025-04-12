8f8fa8cf7d4c59e750abef36318787fd4253f2b9
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-04-11 22:27:17 -0400

    Clean up os_sp.cpp

--- src/recomp_api/os_sp.cpp
+++ src/recomp_api/os_sp.cpp
@@ -1,13 +1,15 @@
-#include "ultramodern/ultramodern.hpp"
 #include "recomp.h"
 
-// TODO: is this necessary?
-uint32_t spStatus = 0;
-
 extern "C" void __osSpSetStatus_recomp(uint8_t* rdram, recomp_context* ctx) {
-    spStatus = ctx->r4;
+    // Ignore sp set status calls
+    //
+    // Most of these calls are avoided by how librecomp implements libultra,
+    // but Dinosaur Planet's scheduler makes a few calls directly to this
+    // during crash handling code, which we can just ignore.
 }
 
 extern "C" void __osSpGetStatus_recomp(uint8_t* rdram, recomp_context* ctx) {
-    ctx->r2 = spStatus;
+    // This is just here for completeness, the game makes no direct calls to
+    // this function. Assert here for sanity's sake.
+    assert(false);
 }
