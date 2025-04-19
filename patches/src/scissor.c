fcde9f6e912170b9a77806be2f98b97cf570b249
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-04-19 14:44:22 -0400

    Partially fix cinematic black bars on widescreen with the existing scissor patches

--- patches/src/scissor.c
+++ patches/src/scissor.c
@@ -3,6 +3,7 @@
 
 extern u32 UINT_800a66f8;
 extern s16 SHORT_8008c524;
+extern s16 SHORT_8008c528;
 extern s32 gCameraSelector;
 
 extern u8 D_800917A0;
@@ -31,6 +32,10 @@ RECOMP_PATCH void func_80002490(Gfx **gdl)
 
     if (UINT_800a66f8 != 0)
     {
+        // This if block is from non-matching code and I don't trust it.
+        // It shouldn't ever run tho...
+        recomp_eprintf("func_80002490: UINT_800a66f8 = %d", UINT_800a66f8);
+
         s32 centerX;
         s32 centerY;
         s32 padX;
@@ -159,7 +164,7 @@ RECOMP_PATCH void func_80037A14(Gfx **gdl, UNK_PTR **mtx, s32 param3) {
             (GPACK_RGBA5551(D_800917A0, D_800917A4, D_800917A8, 1) << 16) 
                 | GPACK_RGBA5551(D_800917A0, D_800917A4, D_800917A8, 1));
 
-        // @recomp: remove hardcoded -1 width/height scissor offset
+        // @recomp: remove hardcoded -1 width/height offset
         if ((param3 & 1) != 0) {
             gDPFillRectangle((*gdl)++, 0, 0, resWidth, resHeight);
             gDLBuilder->needsPipeSync = TRUE;
@@ -173,4 +178,77 @@ RECOMP_PATCH void func_80037A14(Gfx **gdl, UNK_PTR **mtx, s32 param3) {
     }
 
     func_80002490(gdl);
+}
+
+extern f32 gFarPlane;
+extern s16 D_8008C518;
+extern s16 D_8008C51C;
+extern f32 D_800A6270;
+extern f32 D_800A6274;
+extern Camera gCameras[CAMERA_COUNT];
+extern MatrixSlot gMatrixPool[100];
+extern u32 gMatrixCount;
+extern s8 gUseAlternateCamera;
+extern s8 gMatrixIndex;
+
+f32 fexp(f32 x, u32 iterations);
+
+RECOMP_PATCH void camera_tick() {
+    Camera *camera;
+    f32 var4;
+    f32 var5;
+
+    SHORT_8008c524 = SHORT_8008c528;
+    // @recomp: Add back +6 offset removed from other scissor patches
+    // This is necessary for the cinematic top/bottom black bars during cutscnes to be the correct size
+    // TODO: theres probably a better place for this fix, this kinda screws up the animation for the black bars
+    if (SHORT_8008c524 != 0) {
+        SHORT_8008c524 += 6;
+    }
+
+    if (D_8008C518 != 0) {
+        D_8008C518 -= delayByte;
+
+        if (D_8008C518 < 0) {
+            D_8008C518 = 0;
+        }
+
+        gFarPlane = (D_800A6270 - D_800A6274) * ((f32)D_8008C518 / (f32)D_8008C51C) + D_800A6274;
+    }
+
+    gMatrixPool[gMatrixCount].count = -1;
+
+    convert_mtxf_to_mtx_in_pool(gMatrixPool);
+
+    gMatrixCount = 0;
+    gMatrixIndex = 0;
+
+    if (gUseAlternateCamera) {
+        gCameraSelector += 4;
+    }
+    
+    camera = &gCameras[gCameraSelector];
+
+    if (camera->unk_0x5d == 0) {
+        camera->unk_0x5c += -1;
+
+        while (camera->unk_0x5c < 0) {
+            camera->dty = -camera->dty * 0.89999998f;
+
+            camera->unk_0x5c++;
+        }
+    } else if (camera->unk_0x5d == 1) {
+        var4 = fexp(-camera->unk_0x3c * camera->unk_0x38, 20);
+        var5 = fcos16_precise(camera->unk_0x34 * 65535.0f * camera->unk_0x38);
+        var5 *= camera->unk_0x30 * var4;
+
+        camera->dty = var5;
+
+        if (camera->dty < 0.1f && -0.1f < camera->dty) {
+            camera->unk_0x5d = -1;
+            camera->dty = 0.0f;
+        }
+
+        camera->unk_0x38 += delayFloat / 60.0f;
+    }
 }
