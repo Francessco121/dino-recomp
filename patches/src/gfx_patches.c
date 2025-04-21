e1ff2f8763e2436ace066c2ab35d2cd6be702dd1
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-04-20 21:37:09 -0400

    Hack to avoid RT64 descriptor heap crash

--- patches/src/gfx_patches.c
+++ patches/src/gfx_patches.c
@@ -7,6 +7,61 @@
 
 s32 snowbike30FPS = FALSE;
 
+static u16 hack_480pFramebuffers[2][640 * 480];
+
+RECOMP_PATCH void initialize_framebuffers(int someBool, s32 width, s32 height) {
+    VideoResolution *resPtr;
+    u32 hRes;
+    u32 vRes;
+
+    recomp_printf("initialize_framebuffers(%d, %d, %d)\n", someBool, width, height);
+
+    // Get resolution by current video mode
+    resPtr = &gResolutionArray[gVideoMode & 0x7];
+    hRes = resPtr->h;
+    vRes = resPtr->v;
+
+    // Set current resolution
+    gCurrentResolutionH[0] = hRes;
+    gCurrentResolutionH[1] = hRes;
+    gCurrentResolutionV[0] = vRes;
+    gCurrentResolutionV[1] = vRes;
+
+    if (osMemSize != 0x800000) {
+        // No expansion pack detected
+        gFramebufferPointers[0] = (u16*)(FRAMEBUFFER_ADDRESS_NO_EXP_PAK);
+        gFramebufferPointers[1] = (u16*)(FRAMEBUFFER_ADDRESS_NO_EXP_PAK + ((width * height) * 2));
+        
+        gFramebufferStart = (u16*)(FRAMEBUFFER_ADDRESS_NO_EXP_PAK);
+        return;
+    }
+    
+    if (height == 480) {
+        // PAL framebuffer height
+        // gFramebufferPointers[0] = (u16*)(FRAMEBUFFER_ADDRESS_EXP_PAK);
+        // gFramebufferPointers[1] = (u16*)(FRAMEBUFFER_ADDRESS_EXP_PAK + ((width * height) * 2));
+        
+        // gFramebufferStart = (u16*)(FRAMEBUFFER_ADDRESS_EXP_PAK);
+        
+        // @recomp: Use custom addresses for the 640x480 framebuffers to avoid triggering a bug in RT64.
+        // If the 640x480 and 320x260 framebuffers share the same address space, RT64 gets confused and thinks
+        // the old 640x480 framebuffer still exists when the game switches to 260p. This causes a memory leak
+        // that eventually crashes the game. Separating these resolutions avoids the issue.
+        // HACK: remove this when RT64 is patched
+        gFramebufferPointers[0] = (u16*)hack_480pFramebuffers[0];
+        gFramebufferPointers[1] = (u16*)hack_480pFramebuffers[1];
+        
+        gFramebufferStart = gFramebufferPointers[0];
+    } else {
+        // NTSC/M-PAL framebuffer height
+        gFramebufferPointers[0] = (u16*)(FRAMEBUFFER_ADDRESS_EXP_PAK);
+        gFramebufferPointers[1] = (u16*)(FRAMEBUFFER_ADDRESS_EXP_PAK + ((width * height) * 2));
+        
+        gFramebufferEnd = (u16*)(((int) (FRAMEBUFFER_ADDRESS_EXP_PAK + ((width * height) * 2))) + ((width * height) * 2));
+        gFramebufferStart = (u16*)0x80200000;
+    }
+}
+
 RECOMP_PATCH void alloc_frame_buffers(void)
 {   
     // @recomp: Use larger buffer sizes
