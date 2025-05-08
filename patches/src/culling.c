b7007034eeaefcf33f46eee224226bfe622cf60b
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-05-08 19:21:23 -0400

    Only disable culling when not in original aspect ratio mode

--- patches/src/culling.c
+++ patches/src/culling.c
@@ -1,9 +1,10 @@
 #include "culling.h"
+#include "recomp_funcs.h"
 
 #include "PR/ultratypes.h"
 
 s32 recompCullingDisabled = TRUE;
 
 s32 recomp_disable_culling() {
-    return recompCullingDisabled;
+    return recomp_get_aspect_ratio() != RECOMP_ASPECT_ORIGINAL && recompCullingDisabled;
 }
