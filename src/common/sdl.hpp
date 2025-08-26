d8e7b9cb24f894b59960ea87a883628d50b09ee4
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-08-26 01:14:23 -0400

    More SDL stuff

--- src/common/sdl.hpp
+++ src/common/sdl.hpp
@@ -7,4 +7,11 @@
 #else
 #include "SDL2/SDL.h" // IWYU pragma: export
 #include "SDL2/SDL_syswm.h" // IWYU pragma: export
+// Undefine x11 macros that get included by SDL_syswm.h.
+#undef None
+#undef Status
+#undef LockMask
+#undef ControlMask
+#undef Success
+#undef Always
 #endif
