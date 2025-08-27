c1bdc2a7288bbc0bd51136fb27a47d0b73461f50
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2025-08-27 14:54:13 -0400

    WIP widescreen patch for billboard sprites

--- patches/src/culling.c
+++ patches/src/culling.c
@@ -1,6 +1,8 @@
 #include "recomp_funcs.h"
+#include "patches.h"
 
 #include "PR/ultratypes.h"
+#include "sys/gfx/gx.h"
 
 f32 recomp_get_frustum_aspect_hack() {
     f32 recompAspectRatio = recomp_get_aspect_ratio();
@@ -8,4 +10,25 @@ f32 recomp_get_frustum_aspect_hack() {
     // The game usually takes the FOV and divides it by two when creating the culling frustum for 4:3,
     // so we can use some ratios to plug in any aspect ratio.
     return 2.0f * ((4.0f / 3.0f) / recompAspectRatio);
+}
+
+RECOMP_PATCH int is_size_smaller_than_resolution(s32 width, s32 height) {
+    // @recomp: Adjust for recomp aspect ratio. The game thinks we're running at 4:3 so we need this
+    // to return true for negative x values and x values greater than 320, depending on the recomp screen size.
+    // TODO: doesnt seem to work for everything...
+    u32 gameResWidth = gCurrentResolutionH[gFramebufferChoice];
+    u32 gameResHeight = gCurrentResolutionV[gFramebufferChoice];
+
+    s32 ulx = 0;
+    s32 uly = 0;
+    s32 lrx = (s32)gameResWidth;
+    s32 lry = (s32)gameResHeight;
+
+    f32 widthScale = (recomp_get_aspect_ratio() / (4.0f / 3.0f)) - 1.0f;
+    s32 adjust = (s32)((gameResWidth * widthScale) / 2.0f);
+    ulx -= adjust;
+    lrx += adjust;
+
+    return width >= ulx && width < lrx
+        && height >= uly && height < lry;
 }
